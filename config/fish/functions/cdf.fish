12606d3d3d311b4f8170bb75c77f2ab582d4c201
niedhui <dianhui.nie@gmail.com>
2014-07-23 19:11:50 +0800

    more config

---
+++ config/fish/functions/cdf.fish
@@ -0,0 +1,15 @@
+function cdf
+  cd (finder_path )
+end
+
+function finder_path
+    echo '
+    tell application "Finder"
+        if (1 <= (count Finder windows)) then
+            get POSIX path of (target of window 1 as alias)
+        else
+            get POSIX path of (desktop as alias)
+        end if
+    end tell
+    ' | osascript 2>/dev/null -
+end
