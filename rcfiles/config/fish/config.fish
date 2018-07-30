938153962b4f25d0e46b032b5b24441045b10f5c
niedhui <dianhui.nie@gmail.com>
2018-07-30 18:57:26 +0800

    clean

--- rcfiles/config/fish/config.fish
+++ rcfiles/config/fish/config.fish
@@ -16,11 +16,12 @@ alias z="zeus"
 # docker
 alias dc="docker-compose"
 
-
-set -x ANDROID_HOME /Users/niedhui/Library/Android/sdk
-set -x NDK_HOME $ANDROID_HOME/ndk-bundle
-set -x GOPATH /Users/niedhui/workspace/gopath
 set -x EDITOR 'vim'
+
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x NDK_HOME $ANDROID_HOME/ndk-bundle
+set -x GOPATH $HOME/workspace/gopath
+
 set -x SCALA_HOME /usr/local/opt/scala/idea
 set -x PGDATA /usr/local/var/postgres
 
@@ -29,7 +30,7 @@ set PATH /usr/local/sbin $PATH
 set PATH $PATH /usr/local/opt/go/libexec/bin
 set PATH $PATH $HOME/.cargo/bin
 set PATH $ANDROID_HOME/platform-tools $PATH
-set PATH /Users/niedhui/Applications/flutter/bin $PATH
+set PATH $HOME/Applications/flutter/bin $PATH
 
 direnv hook fish | source
 
@@ -39,4 +40,4 @@ set -x LANG C
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-fortune |cowsay
+fortune | cowsay
